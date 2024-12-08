16c5d0a82356ac7d4f27828a9117dbce85e61acb
Stephan Seitz <stephan.seitz@fau.de>
2024-12-08 06:38:51 +0100

    Make day-08 less ugly

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -17,73 +17,12 @@ fn print_grid(grid: &[Vec<char>]) {
     println!();
 }
 
-fn main() -> anyhow::Result<()> {
-    let raw_input = include_str!("../input");
-    //let raw_input = include_str!("../example");
-
-    let grid = raw_input
-        .lines()
-        .filter(|l| !l.is_empty())
-        .map(|l| l.chars().collect_vec())
-        .collect_vec();
-    print_grid(&grid);
-
-    let mut antenas: HashMap<char, HashSet<_>> = HashMap::new();
-
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            let cur = grid[y][x];
-            match cur {
-                '0'..='9' | 'a'..='z' | 'A'..='Z' => {
-                    antenas.entry(cur).or_default().insert((x as i64, y as i64));
-                }
-                _ => (),
-            };
-        }
-    }
-
-    let mut marked_antinodes = grid.clone();
-
-    for (_k, v) in antenas.iter() {
-        for (a, b) in v.iter().tuple_combinations() {
-            let (ax, ay) = *a;
-            let (bx, by) = *b;
-
-            let a_to_b = ((bx - ax), (by - ay));
-
-            let anti1 = (a.0 - a_to_b.0, a.1 - a_to_b.1);
-            let anti2 = (b.0 + a_to_b.0, b.1 + a_to_b.1);
-
-            if anti1.0 >= 0
-                && anti1.0 < grid[0].len() as i64
-                && anti1.1 >= 0
-                && anti1.1 < grid.len() as i64
-            {
-                marked_antinodes[anti1.1 as usize][anti1.0 as usize] = '#';
-            }
-            if anti2.0 >= 0
-                && anti2.0 < grid[0].len() as i64
-                && anti2.1 >= 0
-                && anti2.1 < grid.len() as i64
-            {
-                marked_antinodes[anti2.1 as usize][anti2.0 as usize] = '#';
-            }
-        }
-    }
-    print_grid(&marked_antinodes);
-
-    let mut part1 = 0i64;
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            if marked_antinodes[y][x] == '#' {
-                part1 += 1;
-            }
-        }
-    }
-
-    dbg!(part1);
-
-    let mut marked_antinodes = grid.clone();
+fn count_antinodes(
+    grid: &[Vec<char>],
+    antenas: &HashMap<char, HashSet<(i64, i64)>>,
+    allow_repetitions: bool,
+) -> i64 {
+    let mut marked_antinodes = grid.to_owned();
 
     for (_k, v) in antenas.iter() {
         for (a, b) in v.iter().tuple_combinations() {
@@ -101,6 +40,9 @@ fn main() -> anyhow::Result<()> {
                 && anti1.1 < grid.len() as i64
             {
                 marked_antinodes[anti1.1 as usize][anti1.0 as usize] = '#';
+                if !allow_repetitions {
+                    break;
+                }
                 anti1 = (anti1.0 - a_to_b.0, anti1.1 - a_to_b.1);
             }
             while anti2.0 >= 0
@@ -109,21 +51,51 @@ fn main() -> anyhow::Result<()> {
                 && anti2.1 < grid.len() as i64
             {
                 marked_antinodes[anti2.1 as usize][anti2.0 as usize] = '#';
+                if !allow_repetitions {
+                    break;
+                }
                 anti2 = (anti2.0 + a_to_b.0, anti2.1 + a_to_b.1);
             }
         }
     }
-    print_grid(&marked_antinodes);
+    //print_grid(&marked_antinodes);
 
-    let mut part2 = 0i64;
+    marked_antinodes
+        .iter()
+        .flatten()
+        .filter(|c| **c == '#')
+        .count() as i64
+}
+
+fn main() -> anyhow::Result<()> {
+    let raw_input = include_str!("../input");
+    //let raw_input = include_str!("../example");
+
+    let grid = raw_input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.chars().collect_vec())
+        .collect_vec();
+    //print_grid(&grid);
+
+    let mut antenas: HashMap<char, HashSet<_>> = HashMap::new();
+
     for y in 0..grid.len() {
         for x in 0..grid[0].len() {
-            if marked_antinodes[y][x] == '#' {
-                part2 += 1;
-            }
+            let cur = grid[y][x];
+            match cur {
+                '0'..='9' | 'a'..='z' | 'A'..='Z' => {
+                    antenas.entry(cur).or_default().insert((x as i64, y as i64));
+                }
+                _ => (),
+            };
         }
     }
 
+    let part1 = count_antinodes(&grid, &antenas, false);
+    dbg!(part1);
+
+    let part2 = count_antinodes(&grid, &antenas, true);
     dbg!(part2);
 
     Ok(())
