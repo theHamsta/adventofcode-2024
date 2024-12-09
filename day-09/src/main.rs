1c576e91a2e15e27d66e72fa5cc3443f6409003f
Stephan Seitz <stephan.seitz@fau.de>
2024-12-09 07:45:39 +0100

    Solve very inelegantly

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -7,30 +7,8 @@ use itertools::Itertools;
 use regex::Regex;
 #[allow(unused_imports)]
 use std::collections::HashMap;
-use std::collections::HashSet;
 
-#[allow(dead_code)]
-fn print_grid(grid: &[Vec<char>]) {
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            print!("{}", grid[y][x]);
-        }
-        println!();
-    }
-    println!();
-}
-
-//fn compact(line: &[char]) -> Vec<char>{
-//let mut line = line.to_owned();
-//loop {
-//let first_space = line.iter().position('.').unwrap();
-//let last_digit = line.iter().position('.');
-
-//}
-//line
-//}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct File {
     orig_range: Range<usize>,
     compacted: usize,
@@ -61,59 +39,67 @@ fn main() -> anyhow::Result<()> {
             }
         }
     });
-    //dbg!(&files);
+    for is_part2 in [false, true] {
+        let mut files = files.clone();
+        for space_idx in 0..spaces.len() {
+            let mut file_pos = files.len() - 1;
+            let mut space_size = spaces[space_idx].len();
+            while space_size > 0 {
+                let last_file = &mut files[file_pos];
+                let file_size = last_file.orig_range.iter().len();
+                let to_compact = file_size - last_file.compacted;
 
-    let mut file_pos = files.len() - 1;
+                if spaces[space_idx].start > last_file.orig_range.start {
+                    break;
+                }
 
-    for space_idx in 0..spaces.len() {
-        let mut space_size = spaces[space_idx].len();
-        while space_size > 0 {
-            let last_file = &mut files[file_pos];
-            let file_size = last_file.orig_range.iter().len();
-            let to_compact = file_size - last_file.compacted;
+                let move_size = space_size.min(to_compact);
+                if is_part2 && move_size < file_size {
+                    if file_pos > 0 {
+                        file_pos -= 1;
+                    } else {
+                        break;
+                    }
+                    continue;
+                }
 
-            if spaces[space_idx].start > last_file.orig_range.start {
-                break;
+                last_file.compacted += move_size;
+                last_file
+                    .compacted_positions
+                    .push((spaces[space_idx].start..(spaces[space_idx].start + move_size)).into());
+
+                if last_file.compacted == file_size && file_pos > 0 {
+                    file_pos -= 1;
+                }
+
+                spaces[space_idx].start += move_size;
+                space_size = spaces[space_idx].len();
             }
-
-            let move_size = space_size.min(to_compact);
-
-            last_file.compacted += move_size;
-            last_file
-                .compacted_positions
-                .push((spaces[space_idx].start..(spaces[space_idx].start + move_size)).into());
-
-            if last_file.compacted == file_size && file_pos > 0 {
-                file_pos -= 1;
-            }
-
-            spaces[space_idx].start += move_size;
-            space_size = spaces[space_idx].len();
         }
-    }
-    //dbg!(&files);
+        //dbg!(&files);
 
-    //dbg!(&spaces);
-    let part1: i64 = files
-        .iter()
-        .enumerate()
-        .map(|(id, f)| {
-            let mut sum = 0i64;
-            let uncompacted = f.orig_range.start..(f.orig_range.end - f.compacted);
-            assert_eq!(uncompacted.len() + f.compacted, f.orig_range.iter().len());
-            for pos in uncompacted {
-                sum += (id * pos) as i64;
-            }
-            for r in &f.compacted_positions {
-                for pos in r.iter() {
+        //dbg!(&spaces);
+        let part1: i64 = files
+            .iter()
+            .enumerate()
+            .map(|(id, f)| {
+                let mut sum = 0i64;
+                let uncompacted = f.orig_range.start..(f.orig_range.end - f.compacted);
+                assert_eq!(uncompacted.len() + f.compacted, f.orig_range.iter().len());
+                for pos in uncompacted {
                     sum += (id * pos) as i64;
                 }
-            }
-            sum
-        })
-        .sum();
+                for r in &f.compacted_positions {
+                    for pos in r.iter() {
+                        sum += (id * pos) as i64;
+                    }
+                }
+                sum
+            })
+            .sum();
 
-    dbg!(&part1);
+        dbg!(&part1);
+    }
 
     //let regex = Regex::new(
     //r"([-]?\d+),\s*([-]?\d+),\s*([-]?\d+)\s*@\s*([-]?\d+),\s*([-]?\d+),\s*([-]?\d+)",
