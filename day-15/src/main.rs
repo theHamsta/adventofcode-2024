af4b7f3a93bf55955518e72a1573923de0355172
Stephan Seitz <stephan.seitz@fau.de>
2024-12-15 08:28:05 +0100

    Day 15 ugly but solved

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -7,6 +7,7 @@ use regex::Regex;
 use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::collections::HashSet;
+use std::process::exit;
 use std::thread::sleep_ms;
 
 fn print_grid(grid: &[Vec<char>], pos: (i64, i64)) {
@@ -124,9 +125,17 @@ fn main() -> anyhow::Result<()> {
         }
     }
     //print_grid(&board, start_pos);
+    let mut orig_count = 0i64;
+    for y in 0..board.len() {
+        for x in 0..board[0].len() {
+            if board[y][x] == '[' || board[y][x] == 'O' {
+                orig_count += 1;
+            }
+        }
+    }
 
     let mut pos = start_pos;
-    for mov in movement.chars() {
+    for (step, mov) in movement.chars().enumerate() {
         let dir = match mov {
             '^' => (0, -1),
             '>' => (1, 0),
@@ -141,82 +150,79 @@ fn main() -> anyhow::Result<()> {
             '#' => continue,
             '.' | '@' => pos = neighbor,
             'O' | '[' | ']' => {
-                for i in 2.. {
-                    let after_rock = (pos.0 + i * dir.0, pos.1 + i * dir.1);
-                    let copy = board.clone();
-                    let copy_pos = pos;
-                    match board[after_rock.1 as usize][after_rock.0 as usize] {
-                        '.' | '@' => {
-                            pos = neighbor;
+                let copy = board.clone();
 
-                            let mut to_push = vec![neighbor];
-                            let mut pushed = HashSet::new();
-                            while let Some(b) = to_push.pop() {
-                                if pushed.contains(&b) {
-                                    continue;
-                                } else {
-                                    pushed.insert(b);
-                                }
-
-                                let c = copy[b.1 as usize][b.0 as usize];
-                                if !(c == 'O' || c == '[' || c == ']') {
-                                    continue;
-                                }
-                                let to_overwrite =
-                                    copy[(b.1 + dir.1) as usize][(b.0 + dir.0) as usize];
-                                if to_overwrite == '#' {
-                                    // cancel move, move is blocked
-                                    board = copy;
-                                    pos = copy_pos;
-                                    break;
-                                }
-                                board[(b.1 + dir.1) as usize][(b.0 + dir.0) as usize] =
-                                    copy[b.1 as usize][b.0 as usize];
-                                if !pushed.contains(&(b.0 - dir.0, b.1 - dir.1)) {
-                                    board[b.1 as usize][b.0 as usize] = '.';
-                                }
-
-                                match copy[b.1 as usize][b.0 as usize] {
-                                    'O' => {
-                                        to_push.push((b.0 + dir.0, b.1 + dir.1));
-                                    }
-                                    '[' => {
-                                        to_push.push((b.0 + dir.0, b.1 + dir.1));
-                                        if dir != (1, 0) {
-                                            to_push.push((b.0 + 1, b.1));
-                                        }
-                                    }
-                                    ']' => {
-                                        to_push.push((b.0 + dir.0, b.1 + dir.1));
-                                        if dir != (-1, 0) {
-                                            to_push.push((b.0 - 1, b.1));
-                                        }
-                                    }
-                                    _ => (),
-                                }
-                            }
-                            break;
-                        }
-                        'O' | '[' | ']' => {
-                            continue;
-                        }
-                        '#' => break,
-                        c => panic!("Reached unexpected char {c:?}"),
+                let mut to_push = vec![neighbor];
+                let mut pushed = HashSet::new();
+                let mut move_canceled = false;
+                while let Some(b) = to_push.pop() {
+                    let c = copy[b.1 as usize][b.0 as usize];
+                    if !(c == 'O' || c == '[' || c == ']') {
+                        continue;
                     }
+                    if pushed.contains(&b) {
+                        continue;
+                    } else {
+                        pushed.insert(b);
+                    }
+
+                    let to_overwrite = copy[(b.1 + dir.1) as usize][(b.0 + dir.0) as usize];
+                    if to_overwrite == '#' {
+                        // cancel move, move is blocked
+                        board = copy;
+                        move_canceled = true;
+                        break;
+                    }
+                    board[(b.1 + dir.1) as usize][(b.0 + dir.0) as usize] =
+                        copy[b.1 as usize][b.0 as usize];
+                    if !pushed.contains(&(b.0 - dir.0, b.1 - dir.1)) {
+                        board[b.1 as usize][b.0 as usize] = '.';
+                    }
+
+                    match copy[b.1 as usize][b.0 as usize] {
+                        'O' => {
+                            to_push.push((b.0 + dir.0, b.1 + dir.1));
+                        }
+                        '[' => {
+                            to_push.push((b.0 + dir.0, b.1 + dir.1));
+                            to_push.push((b.0 + 1, b.1));
+                        }
+                        ']' => {
+                            to_push.push((b.0 + dir.0, b.1 + dir.1));
+                            to_push.push((b.0 - 1, b.1));
+                        }
+                        _ => (),
+                    }
+                }
+                if !move_canceled {
+                    pos = neighbor;
                 }
             }
             c => panic!("Reached unexpected char {c:?}"),
         }
-        //println!("{}", mov);
+        let mut count = 0i64;
+        for y in 0..board.len() {
+            for x in 0..board[0].len() {
+                if board[y][x] == '[' || board[y][x] == 'O' {
+                    count += 1;
+                }
+            }
+        }
+        if count != orig_count {
+            dbg!(&count);
+            println!("{} {}/{}", mov, step, movement.len());
+            print_grid(&board, pos);
+            exit(1);
+        }
+        //println!("{} {}/{}", mov, step, movement.len());
         //print_grid(&board, pos);
-        //sleep_ms(250);
     }
     //print_grid(&board, pos);
     let mut part2: i64 = 0;
 
     for y in 0..board.len() {
         for x in 0..board[0].len() {
-            if board[y][x] == '['  {
+            if board[y][x] == '[' || board[y][x] == 'O' {
                 part2 += x as i64 + 100 * y as i64;
             }
         }
