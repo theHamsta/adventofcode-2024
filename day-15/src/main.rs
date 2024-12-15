3ee2d67a20ffc83b23e52b821a6879fffecdbe0d
Stephan Seitz <stephan.seitz@fau.de>
2024-12-15 08:38:06 +0100

    Clean up a bit (still ugly)

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -8,7 +8,6 @@ use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::collections::HashSet;
 use std::process::exit;
-use std::thread::sleep_ms;
 
 fn print_grid(grid: &[Vec<char>], pos: (i64, i64)) {
     for y in 0..grid.len() {
@@ -24,97 +23,9 @@ fn print_grid(grid: &[Vec<char>], pos: (i64, i64)) {
     println!();
 }
 
-fn main() -> anyhow::Result<()> {
-    let raw_input = include_str!("../input");
-    //let raw_input = include_str!("../example1");
-    let mut it = raw_input.split("\n\n");
-    let board = it.next().unwrap();
-    let movement = it.next().unwrap();
-    //dbg!(&board);
-    //dbg!(&movement);
-
-    let input = board
-        .lines()
-        .map(|l| l.trim().chars().collect_vec())
-        .collect_vec();
-
-    let mut board = input.clone();
+fn solve(board: &[Vec<char>], movement: &str, debug: bool) -> i64 {
+    let mut board = board.to_owned();
     let mut start_pos = (0i64, 0i64);
-
-    'outer: for y in 0..input.len() {
-        for x in 0..input[0].len() {
-            if input[y][x] == '@' {
-                start_pos = (x as i64, y as i64);
-                break 'outer;
-            }
-        }
-    }
-    //print_grid(&input, start_pos);
-
-    let mut pos = start_pos;
-    for mov in movement.chars() {
-        let dir = match mov {
-            '^' => (0, -1),
-            '>' => (1, 0),
-            'v' => (0, 1),
-            '<' => (-1, 0),
-            '\n' => continue,
-            c => panic!("Reached unexpected char {c:?}"),
-        };
-
-        let neighbor = (pos.0 + dir.0, pos.1 + dir.1);
-        match board[neighbor.1 as usize][neighbor.0 as usize] {
-            '#' => continue,
-            '.' | '@' => pos = neighbor,
-            'O' => {
-                for i in 2.. {
-                    let after_rock = (pos.0 + i * dir.0, pos.1 + i * dir.1);
-                    match board[after_rock.1 as usize][after_rock.0 as usize] {
-                        '.' | '@' => {
-                            pos = neighbor;
-                            board[after_rock.1 as usize][after_rock.0 as usize] = 'O';
-                            board[neighbor.1 as usize][neighbor.0 as usize] = '.';
-                            break;
-                        }
-                        'O' => {
-                            continue;
-                        }
-                        '#' => break,
-                        c => panic!("Reached unexpected char {c:?}"),
-                    }
-                }
-            }
-            c => panic!("Reached unexpected char {c:?}"),
-        }
-        //println!("{}", mov);
-        //print_grid(&board, pos);
-    }
-
-    let mut part1: i64 = 0;
-
-    for y in 0..board.len() {
-        for x in 0..board[0].len() {
-            if board[y][x] == 'O' {
-                part1 += x as i64 + 100 * y as i64;
-            }
-        }
-    }
-    dbg!(&part1);
-
-    let mut board = input
-        .iter()
-        .map(|l| {
-            l.iter()
-                .flat_map(|c| match c {
-                    '#' => ['#', '#'],
-                    '@' => ['@', '.'],
-                    '.' => ['.', '.'],
-                    'O' => ['[', ']'],
-                    c => panic!("Reached unexpected char {c:?}"),
-                })
-                .collect_vec()
-        })
-        .collect_vec();
 
     'outer: for y in 0..board.len() {
         for x in 0..board[0].len() {
@@ -124,12 +35,15 @@ fn main() -> anyhow::Result<()> {
             }
         }
     }
+
     //print_grid(&board, start_pos);
     let mut orig_count = 0i64;
-    for y in 0..board.len() {
-        for x in 0..board[0].len() {
-            if board[y][x] == '[' || board[y][x] == 'O' {
-                orig_count += 1;
+    if debug {
+        for y in 0..board.len() {
+            for x in 0..board[0].len() {
+                if board[y][x] == '[' || board[y][x] == 'O' {
+                    orig_count += 1;
+                }
             }
         }
     }
@@ -200,33 +114,72 @@ fn main() -> anyhow::Result<()> {
             }
             c => panic!("Reached unexpected char {c:?}"),
         }
-        let mut count = 0i64;
-        for y in 0..board.len() {
-            for x in 0..board[0].len() {
-                if board[y][x] == '[' || board[y][x] == 'O' {
-                    count += 1;
+        if debug {
+            let mut count = 0i64;
+            for y in 0..board.len() {
+                for x in 0..board[0].len() {
+                    if board[y][x] == '[' || board[y][x] == 'O' {
+                        count += 1;
+                    }
                 }
             }
+            if count != orig_count {
+                dbg!(&count);
+                println!("{} {}/{}", mov, step, movement.len());
+                print_grid(&board, pos);
+                exit(1);
+            }
+            //println!("{} {}/{}", mov, step, movement.len());
+            //print_grid(&board, pos);
         }
-        if count != orig_count {
-            dbg!(&count);
-            println!("{} {}/{}", mov, step, movement.len());
-            print_grid(&board, pos);
-            exit(1);
-        }
-        //println!("{} {}/{}", mov, step, movement.len());
-        //print_grid(&board, pos);
     }
     //print_grid(&board, pos);
-    let mut part2: i64 = 0;
+    let mut sum: i64 = 0;
 
     for y in 0..board.len() {
         for x in 0..board[0].len() {
             if board[y][x] == '[' || board[y][x] == 'O' {
-                part2 += x as i64 + 100 * y as i64;
+                sum += x as i64 + 100 * y as i64;
             }
         }
     }
+    sum
+}
+
+fn main() -> anyhow::Result<()> {
+    let raw_input = include_str!("../input");
+    //let raw_input = include_str!("../example1");
+    let mut it = raw_input.split("\n\n");
+    let board = it.next().unwrap();
+    let movement = it.next().unwrap();
+    //dbg!(&board);
+    //dbg!(&movement);
+
+    let board = board
+        .lines()
+        .map(|l| l.trim().chars().collect_vec())
+        .collect_vec();
+
+    let debug = false;
+    let part1 = solve(&board, movement, debug);
+    dbg!(&part1);
+
+    let board = board
+        .iter()
+        .map(|l| {
+            l.iter()
+                .flat_map(|c| match c {
+                    '#' => ['#', '#'],
+                    '@' => ['@', '.'],
+                    '.' => ['.', '.'],
+                    'O' => ['[', ']'],
+                    c => panic!("Reached unexpected char {c:?}"),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let part2 = solve(&board, movement, debug);
     dbg!(&part2);
 
     Ok(())
